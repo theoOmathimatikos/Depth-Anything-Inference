ead5aa42f7195b7bff651f5760a14972cdee9f16
theoOmathimatikos <theolyber@gmail.com>
2024-11-07 15:20:21 +0200

    Adding zip file on github

--- Depth_Anything/depth_anything/dpt.py
+++ Depth_Anything/depth_anything/dpt.py
@@ -137,6 +137,7 @@ class DPTHead(nn.Module):
         
         
 class DPT_DINOv2(nn.Module):
+    
     def __init__(self, encoder='vitl', features=256, out_channels=[256, 512, 1024, 1024], use_bn=False, use_clstoken=False, localhub=True):
         super(DPT_DINOv2, self).__init__()
         
